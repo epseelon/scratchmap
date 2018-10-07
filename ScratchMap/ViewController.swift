a4064f49f7012c20df15f22e9ee5eea6aafa71d3
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2018-10-07 11:08:40 +0200

    Initial Commit

---
+++ ScratchMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScratchMap
+//
+//  Created by Sebastien on 07/10/2018.
+//  Copyright © 2018 Epseelon sprl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
